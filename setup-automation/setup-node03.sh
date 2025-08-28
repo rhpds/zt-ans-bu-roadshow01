cd95a7ab5132842182d3d3f6d1de7c03975b1544
Nuno Martins <nmartins@redhat.com>
2025-08-28 17:15:05 +0200

    Update setup-node03.sh

--- setup-automation/setup-node03.sh
+++ setup-automation/setup-node03.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+retry() {
+    for i in {1..3}; do
+        echo "Attempt $i: $2"
+        if $1; then
+            return 0
+        fi
+        [ $i -lt 3 ] && sleep 5
+    done
+    echo "Failed after 3 attempts: $2"
+    exit 1
+}
 
-curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
-update-ca-trust
-rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
+retry "curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt"
+retry "update-ca-trust"
+retry "rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm"
+retry "subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}"
+retry "dnf install httpd nano xfsdump -y"
 
-subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
+# curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+# update-ca-trust
+# rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 
-dnf install httpd nano -y
+# subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
 
-
+# dnf install httpd nano -y
 
 cat <<EOF | tee /var/www/html/index.html
 
@@ -44,7 +59,7 @@ cat <<EOF | tee /var/www/html/index.html
 
 EOF
 
-systemctl start httpd
+retry "systemctl start httpd"
 
 mkdir /backup
 chmod -R 777 /backup
